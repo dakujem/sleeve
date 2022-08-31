a9cde400085f6bc4e178c8bf2c4f050295a8eb58
Andrej Rypo <xrypak@gmail.com>
2022-08-31 13:13:11 +0200

    Fix PHP support (7.4 & 8+)
    added type-hints, strict types

--- src/Sleeve.php
+++ src/Sleeve.php
@@ -1,11 +1,11 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Dakujem;
 
 use Pimple\Container as Pimple;
 use Psr\Container\ContainerInterface;
-
 
 /**
  * A trivial extension of Pimple Container.
@@ -14,54 +14,51 @@ use Psr\Container\ContainerInterface;
  */
 class Sleeve extends Pimple implements ContainerInterface
 {
-
-    public function has($id)
+    public function has(string $id): bool
     {
         return $this->offsetExists($id);
     }
 
-
-    public function get($id)
+    #[\ReturnTypeWillChange]
+    public function get(string $id)//: mixed
     {
         return $this->offsetGet($id);
     }
 
-
-    public function set($id, $value)
+    #[\ReturnTypeWillChange]
+    public function set($id, $value)//: mixed
     {
         $this->offsetSet($id, $value);
         return $this;
     }
 
-
-    public function unset($id)
+    public function unset($id): self
     {
         $this->offsetUnset($id);
         return $this;
     }
 
-
-    public function __get($id)
+    #[\ReturnTypeWillChange]
+    public function __get($id)//: mixed
     {
-        return $this->get($id);
+        return $this->get((string)$id);
     }
 
-
-    public function __set($id, $value)
+    #[\ReturnTypeWillChange]
+    public function __set($id, $value)//: mixed
     {
         return $this->set($id, $value);
     }
 
-
-    public function __isset($id)
+    #[\ReturnTypeWillChange]
+    public function __isset($id): bool
     {
-        return $this->has($id);
+        return $this->has((string)$id);
     }
 
-
-    public function __unset($id)
+    #[\ReturnTypeWillChange]
+    public function __unset($id): void
     {
         $this->offsetUnset($id);
     }
-
 }
